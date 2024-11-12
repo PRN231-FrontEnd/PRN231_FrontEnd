4dc7e91f60b76a7bfe7bc49461c42d35ac271563
TyKay264 <khaipt4working@gmail.com>
2024-11-12 11:30:10 +0700

    pull

--- src/pages/SuccessNoti/index.jsx
+++ src/pages/SuccessNoti/index.jsx
@@ -3,11 +3,9 @@ import "./style.css";
 import { useLocation } from "react-router-dom";
 
 const SuccessNoti = () => {
-    // Lấy query từ URL
     const query = new URLSearchParams(window.location.search);
     const transactionStatus = query.get("vnp_TransactionStatus");
 
-    // Kiểm tra nếu transactionStatus != "00" thì thất bại, nếu == "00" thì thành công
     const isSuccess = transactionStatus === "00";
 
     return (
@@ -21,14 +19,14 @@ const SuccessNoti = () => {
                             Thank you for your payment. We will <br />
                             be in contact with more details shortly.
                         </p>
-                        <button className="btn btn-success">Go to Dashboard</button>
+                        <button className="btn btn-success" href="/">Go to homepage</button>
                     </div>
                 ) : (
                     <div className="message-box _failed">
                         <i className="fa fa-times-circle" aria-hidden="true" />
                         <h2>Payment Failed</h2>
                         <p>Please try again later.</p>
-                        <button className="btn btn-danger">Retry Payment</button>
+                        <button className="btn btn-danger" href="/">Retry Payment</button>
                     </div>
                 )}
             </div>
